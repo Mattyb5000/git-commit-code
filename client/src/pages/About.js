46833225ecb7330bc6b96644806c60a4a0af18b7
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-25 14:58:55 -0400

    stuff and thangs

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import Navbar1 from '../components/Navbar1/Navbar';
+import Navbar1 from '../components/Navbar1/Navbar1';
 
 function About() {
   return (
