f5c4402c921ff6e9a10f38aa2c9833b16c420868
Liz Emerson <uknity@gmail.com>
2021-05-25 11:55:10 -0400

    created 2 nav bars - 1 for home and 1 for other pages; moved background pic to home page only

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Navbar2 from "../components/Navbar2/Navbar"
+
 const Profile = () => {
   
     return (
       <div>
+        <Navbar2 />
        Hello!
        </div>
     );
---
+++ client/src/pages/ReactProjects.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar2 from "../components/Navbar2/Navbar";
+import ProjectCard from "../components/ProjectCard/index.js";
+import ProjectTitle from "../components/ProjectTitle/index.js";
+
+const ReactProjects = () => {
+  
+    return (
+      <div>
+        <Navbar2 />
+       Hello!
+       </div>
+    );
+  }
+
+
+export default ReactProjects;
