1fd3ffce4aa1f59cf614a5bc39ae79f388699232
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-26 17:25:51 -0400

    added react-chartjs2 to dependencies and changed to class component

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Bar} from 'react-chartjs-2';
 
-function Chart() {
-    return (
-        <div>
-        <canvas id="myChart"></canvas>
-        Hello chart!
-      </div>
-    )
+class Chart extends Component{
+    render(){
+        return (
+            <div className="chart">
+                 CHART COMPONENT! LEMME HOLLA AT YA!!!
+            </div>
+        )
+    }
 }
 
 export default Chart;
