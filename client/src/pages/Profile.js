2343d268f34606361c95866cb76f6b679fb6a987
Liz Emerson <uknity@gmail.com>
2021-05-26 10:43:25 -0400

    fake user array; began profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,26 +2,37 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Navbar2 from "../components/Navbar2/Navbar2";
 import ProjectCard from "../components/ProjectCard/index.js";
-import FakeAPI from "../utils/FakeAPI";
+import FakeUserAPI from "../utils/FakeUserAPI";
 
 const Profile = () => {
-  const [projects, setProjects] = useState([]);
+	const [userProjects, setUserProjects] = useState([]);
 
 	// loads all projects and sets them to projects
 	useEffect(() => {
-		loadProjects();
+		loadUserProjects();
 	}, []);
 
-	function loadProjects() {
-    // store date from api call in a variable that is an array
-    const tempArray = FakeAPI.getProjects();
-    const jsArray = tempArray.filter(jsProj => jsProj.language === "javascript");
-    console.log(jsArray);
+	function loadUserProjects() {
+		// store date from api call in a variable that is an array
+		const tempUserArray = FakeUserAPI.getUserProjects();
+    console.log(tempUserArray);
+		const user = tempUserArray.filter(
+			(user) => user.user_id === 1
+      // {(req.session.user_id)}
+		);
+    
+		console.log(user);
+    // const userProj = user.projects.map(project => (
+    //   for (i=0; i < userProj.length; i++) {
 
-    //sort data in that variable array
-    // pass sorted array to setProjects function
-    setProjects(jsArray); 
-    //once I'm using the API, the projects need to be sorted in setProjects function within curly braces
+    // }...userProj
+    // ) );
+
+    // console.log(userProj);
+		//sort data in that variable array
+		// pass sorted array to setProjects function
+		// setUserProjects(userProj);
+		//once I'm using the API, the projects need to be sorted in setProjects function within curly braces
 		/*	//.then((res) => setProjects(res.data))
 			.catch((err) => {
 				console.log(err);
@@ -29,24 +40,49 @@ const Profile = () => {
 	}
 	return (
 		<div>
-      <Navbar2 />
-      <h5 className="pageTitle text-center pt-5">My Profile</h5>
-      <div className="container-fluid">
-        <div className="row d-flex justify-content-around">
-          
-
-        </div>
-      
-      
-
-
-      </div>
-			
-			
-			
+			<Navbar2 />
+			{/* <h5 className="pageTitle text-center pt-5">My Profile</h5>
+			<div className="container-fluid">
+				<div className="row d-flex">
+					<div className="col-sm-7">
+						<table class="table">
+							<thead>
+								<tr>
+									<th scope="col">Project Number</th>
+									<th scope="col">Project Name</th>
+                  <th scope="col">Project Language</th>
+									<th scope="col">Project Link</th>
+                  <th scope="col">Submit Completed Project</th>
+									
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Mark</td>
+									<td>Otto</td>
+									<td>@mdo</td>
+                  <td></td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>Jacob</td>
+									<td>Thornton</td>
+									<td>@fat</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>Larry</td>
+									<td>the Bird</td>
+									<td>@twitter</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div> */}
 		</div>
 	);
 };
 
 export default Profile;
-
