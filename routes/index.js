df7a7c2c655e6cfc29292221ed3322a429f3b454
Liz Emerson <uknity@gmail.com>
2021-05-26 14:30:27 -0400

    revised routes index.js and added index.js to api routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
-const projectRoutes = require("./projectRoutes");
-const userRoutes = require("./userRoutes");
+const path = require("path");
+// const projectRoutes = require("./projectRoutes");
+// const userRoutes = require("./userRoutes");
+const apiRoutes = require("./api");
+
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 // project routes
-router.use("/projects", projectRoutes);
+// router.use("/projects", projectRoutes);
 
 //user routes
-router.use("/user", userRoutes)
+// router.use("/user", userRoutes)
+
+//
 
 module.exports = router;
