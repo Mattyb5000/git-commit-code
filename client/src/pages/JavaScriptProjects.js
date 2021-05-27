0bb8e17e3a6e7610fbf745f1906faa1d8b872aa5
Liz Emerson <uknity@gmail.com>
2021-05-27 13:45:07 -0400

    project route errors corrected on js projects page

--- client/src/pages/JavaScriptProjects.js
+++ client/src/pages/JavaScriptProjects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Navbar2 from "../components/Navbar2/Navbar2";
 import ProjectCard from "../components/ProjectCard/index.js";
-import API from "../utils/API.js"
+import API from "../utils/API"
 
 const JavaScriptProjects = () => {
 	// sets initial state of projects
@@ -15,23 +15,17 @@ const JavaScriptProjects = () => {
 
 	const loadProjects =() => {
 		// store date from api call in a variable that is an array
+		console.log('you are in the loadProjects function');
 		API.getProjects()
-		.then(res =>
-			console.log(res)
-			// setProjects(res.filter((jsProj) => jsProj.language === "Javascript"))
-			// console.log(res)
-			
-			)
-		console.log(projects);
+		.then((res) => {
+		console.log(res);
+		const jsArray = () => {
+			res.filter((jsProj) => jsProj.language === "Javascript");
+		};
+		console.log(jsArray);
+		})
+		.catch((err) => console.log(err))	
 	}
-		// const jsArray = tempArray.filter(
-		// 	(jsProj) => jsProj.language === "javascript"
-		// );
-		
-
-		//sort data in that variable array
-		// pass sorted array to setProjects function
-		// setProjects(jsArray);
 		//once I'm using the API, the projects need to be sorted in setProjects function within curly braces
 		/*	//.then((res) => setProjects(res.data))
 			.catch((err) => {
@@ -45,7 +39,7 @@ const JavaScriptProjects = () => {
 	return (
 		<div>
 			<Navbar2 />
-			<h5 className="pageTitle text-center pt-5">JavaScript Projects</h5>
+			{/* <h5 className="pageTitle text-center pt-5">JavaScript Projects</h5>
 			<div className="container-fluid">
 				<div className="row d-flex justify-content-around">
 						{projects.map((jsProj) => (
@@ -53,7 +47,7 @@ const JavaScriptProjects = () => {
 						))}
 					
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const projectRoutes = require("./projectsRoutes");
+const projectRoutes = require("./projectRoutes");
 
-// Book routes
+
 router.use("/users", userRoutes);
-
-// Google Routes
 router.use("/projects", projectRoutes);
 
 // For anything else, render the html page
