44d2c4b912433816951ad3145cf5f0fa2b4b4a78
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-26 22:05:29 -0400

    got chart to show, fixed axios api route and router in index.js

--- models/project.js
+++ models/project.js
@@ -7,6 +7,7 @@ const projectSchema = new Schema({
   title: { type: String, required: true },
   link: { type: String, required: true },
   image: { type: String, required: true },
+  // completed: (default to false, true would be completed and data would go into the completed bar in the graph)
 });
 
 const Project = mongoose.model("Project", projectSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const projectRoutes = require("./projects");
+const projectRoutes = require("./api/projectRoutes");
 
 // project routes
-router.use("/projects", projectRoutes);
+router.use("/api/projects", projectRoutes);
 
 module.exports = router;
