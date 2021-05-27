517660440845d48b02567f824ce19f5daf0c67d4
Liz Emerson <uknity@gmail.com>
2021-05-27 08:00:48 -0400

    modified projectRoutes, UserRoutes, projectsController, & userController

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -6,32 +6,32 @@ module.exports = {
       db.Project
         .find(req.query)
         .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
+        .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
       db.Project
         .findById(req.params.id)
-        .then(dbModel => res.json(dbModel))
+        .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
       db.Project
         .create(req.body)
-        .then(dbModel => res.json(dbModel))
+        .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
       db.Project
         .findOneAndUpdate({ _id: req.params.id }, req.body)
-        .then(dbModel => res.json(dbModel))
+        .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
       db.Project
         .findById({ _id: req.params.id })
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
+        .then(dbProject => dbProject.remove())
+        .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     }
   };
