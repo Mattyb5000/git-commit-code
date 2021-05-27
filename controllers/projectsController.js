811f6ac03eccf73e00d2768e48e9ee5994344e7e
Liz Emerson <uknity@gmail.com>
2021-05-27 10:47:51 -0400

    added console log features to api routes

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,6 +3,7 @@ const db = require("../models");
 // Defining methods for the projectsController-ST
 module.exports = {
     findAll: function(req, res) {
+      console.log('you are in api find all project route');
       db.Project
         .find(req.query)
         .sort({ date: -1 })
@@ -10,24 +11,28 @@ module.exports = {
         .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
+      console.log('you are in api findById project route');
       db.Project
         .findById(req.params.id)
         .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
+      console.log('you are in api create project route');
       db.Project
         .create(req.body)
         .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
+      console.log('you are in api update project route');
       db.Project
         .findOneAndUpdate({ _id: req.params.id }, req.body)
         .then(dbProject => res.json(dbProject))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
+      console.log('you are in api remove project route');
       db.Project
         .findById({ _id: req.params.id })
         .then(dbProject => dbProject.remove())
