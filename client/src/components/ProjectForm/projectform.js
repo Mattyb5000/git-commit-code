4975e914316313d38ddcf6ca6037091f951863ef
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-28 18:51:12 -0400

    fixed submit project button within container, still need to center

--- client/src/components/ProjectForm/projectform.js
+++ client/src/components/ProjectForm/projectform.js
@@ -18,7 +18,7 @@ export function TextArea(props) {
 
 export function FormBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success position-static">
       {props.children}
     </button>
   );
