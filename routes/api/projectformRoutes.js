9de2002775e60ed38b807a165597bfbe0638250c
Liz Emerson <uknity@gmail.com>
2021-05-28 13:58:48 -0400

    merging troubleshooting

--- routes/api/projectformRoutes.js
+++ routes/api/projectformRoutes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const projectsFormController = require("../../controllers/projectFormController");
+const projectFormController = require("../../controllers/projectFormController");
 
 // Matches with "/api/projects"
 router.route("/")
-  .get(projectsFormController.find)
-  .post(projectsFormController.create);
+  .get(projectFormController.find)
+  .post(projectFormController.create);
 
 // Matches with "/api/projects/:id"
 router
   .route("/:id")
-  .get(projectsFormController.findById)
-  .put(projectsFormController.update)
-  .delete(projectsFormController.remove);
+  .get(projectFormController.findById)
+  .put(projectFormController.update)
+  .delete(projectFormController.remove);
 
 module.exports = router;
