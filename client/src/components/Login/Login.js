424720bfa11e7f36a4bf34677e0e764ee47429b7
Seth Torek <seth.torek@gmail.com>
2021-05-28 15:43:23 -0400

    worked on login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,127 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Navbar2 from "../Navbar2/Navbar2";
 
-
-
-
-
 function Login() {
-  return (
+  // use state for login and signup functionality
 
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function signupFunc() {
+    setLoggedIn(true);
+    console.log("logged in");
+  }
+
+  function signinFunc() {
+    setLoggedIn(true);
+    console.log("signed up");
+  }
+
+  return (
     <div className="background">
       <Navbar2 />
       <div className="container col-9 animate__animated animate__fadeIn">
         <div className="row">
           {/* Signup */}
           <div className="form col-6">
-          <h1 className="title">Signup</h1>
-        <form>
-          <div class="form-outline mb-4">
-            <input type="email" id="form1Example1" class="form-control" />
-            <label class="form-label" for="form1Example1">
-              Email address
-            </label>
-          </div>
-          <div class="form-outline mb-4">
-            <input type="firstName" id="form1Example1" class="form-control" />
-            <label class="form-label" for="form1Example1">
-              First Name
-            </label>
-          </div>
-          <div class="form-outline mb-4">
-            <input type="lastName" id="form1Example1" class="form-control" />
-            <label class="form-label" for="form1Example1">
-              Last Name
-            </label>
-          </div>
-
-          <div class="form-outline mb-4">
-            <input type="password" id="form1Example2" class="form-control" />
-            <label class="form-label" for="form1Example2">
-              Password
-            </label>
-          </div>
-
-          <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-              <div class="form-check">
+            <h1 className="title">Signup</h1>
+            <form>
+              <div class="form-outline mb-4">
                 <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="form1Example3"
-                  checked
+                  type="email"
+                  id="form1Example1"
+                  class="form-control"
+                  id="lastName-signup"
                 />
-                <label class="form-check-label" for="form1Example3">
-                  {" "}
-                  Remember me{" "}
+                <label class="form-label" for="form1Example1">
+                  Email address
                 </label>
               </div>
-            </div>
-
-            <div class="col">
-              <a href="#!">Forgot password?</a>
-            </div>
-          </div>
-
-          <button type="submit" class="signinBtn btn btn-primary btn-block">
-            Sign Up
-          </button>
-        </form>
-        </div> 
-
-        {/* login */}
-        <div className="col-6">
-        <h1 className="title">Login</h1>
-        <form>
-          <div class="form-outline mb-4">
-            <input type="email" id="form1Example1" class="form-control" />
-            <label class="form-label" for="form1Example1">
-              Email address
-            </label>
-          </div>
-
-          <div class="form-outline mb-4">
-            <input type="password" id="form1Example2" class="form-control" />
-            <label class="form-label" for="form1Example2">
-              Password
-            </label>
-          </div>
-
-          <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-              <div class="form-check">
+              <div class="form-outline mb-4">
                 <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="form1Example3"
-                  checked
+                  type="firstName"
+                  id="form1Example1"
+                  class="form-control"
+                  id="firstName-signup"
                 />
-                <label class="form-check-label" for="form1Example3">
-                  {" "}
-                  Remember me{" "}
+                <label class="form-label" for="form1Example1">
+                  First Name
                 </label>
               </div>
-            </div>
+              <div class="form-outline mb-4">
+                <input
+                  type="lastName"
+                  id="form1Example1"
+                  class="form-control"
+                  id="lastName-signup"
+                />
+                <label class="form-label" for="form1Example1">
+                  Last Name
+                </label>
+              </div>
 
-            <div class="col">
-              <a href="#!">Forgot password?</a>
-            </div>
+              <div class="form-outline mb-4">
+                <input
+                  type="password"
+                  id="form1Example2"
+                  class="form-control"
+                  id="password-signup"
+                />
+                <label class="form-label" for="form1Example2">
+                  Password
+                </label>
+              </div>
+
+              <div class="row mb-4">
+                <div class="col d-flex justify-content-center">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="form1Example3"
+                      checked
+                    />
+                    <label class="form-check-label" for="form1Example3">
+                      {" "}
+                      Remember me{" "}
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <a href="#!">Forgot password?</a>
+                </div>
+              </div>
+
+              <button
+                onClick={signupFunc}
+                type="submit"
+                class="signinBtn btn btn-primary btn-block"
+                id="signup-form"
+              >
+                Sign Up
+              </button>
+            </form>
           </div>
-          <button type="submit" class=" signinBtn btn btn-primary btn-block">
-            Sign in
-          </button>
-        </form>
-        </div>
-        
-          
 
-        </div>
+          {/* login */}
+          <div className="col-6">
+            <h1 className="title">Login</h1>
+            <form>
+              <div class="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form1Example1"
+                  class="form-control"
+                  id="email-login"
+                />
+                <label class="form-label" for="form1Example1">
+                  Email address
+                </label>
+              </div>
 
+              <div class="form-outline mb-4">
+                <input
+                  type="password"
+                  id="form1Example2"
+                  class="form-control"
+                  id="password-login"
+                />
+                <label class="form-label" for="form1Example2">
+                  Password
+                </label>
+              </div>
+
+              <div class="row mb-4">
+                <div class="col d-flex justify-content-center">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="form1Example3"
+                      checked
+                    />
+                    <label class="form-check-label" for="form1Example3">
+                      {" "}
+                      Remember me{" "}
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <a href="#!">Forgot password?</a>
+                </div>
+              </div>
+              <button
+                onClick={signinFunc}
+                type="submit"
+                class=" signinBtn btn btn-primary btn-block"
+                id="login-form"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      
     </div>
 
     // <div className="background">
@@ -129,110 +173,107 @@ function Login() {
     // <div className="container col-3">
     // <div className="row">
     //   <div className="signup animate__animated animate__fadeInLeft">
-        // <h1 className="title">Signup</h1>
-        // <form>
-        //   <div class="form-outline mb-4">
-        //     <input type="email" id="form1Example1" class="form-control" />
-        //     <label class="form-label" for="form1Example1">
-        //       Email address
-        //     </label>
-        //   </div>
-        //   <div class="form-outline mb-4">
-        //     <input type="firstName" id="form1Example1" class="form-control" />
-        //     <label class="form-label" for="form1Example1">
-        //       First Name
-        //     </label>
-        //   </div>
-        //   <div class="form-outline mb-4">
-        //     <input type="lastName" id="form1Example1" class="form-control" />
-        //     <label class="form-label" for="form1Example1">
-        //       Last Name
-        //     </label>
-        //   </div>
+    // <h1 className="title">Signup</h1>
+    // <form>
+    //   <div class="form-outline mb-4">
+    //     <input type="email" id="form1Example1" class="form-control" />
+    //     <label class="form-label" for="form1Example1">
+    //       Email address
+    //     </label>
+    //   </div>
+    //   <div class="form-outline mb-4">
+    //     <input type="firstName" id="form1Example1" class="form-control" />
+    //     <label class="form-label" for="form1Example1">
+    //       First Name
+    //     </label>
+    //   </div>
+    //   <div class="form-outline mb-4">
+    //     <input type="lastName" id="form1Example1" class="form-control" />
+    //     <label class="form-label" for="form1Example1">
+    //       Last Name
+    //     </label>
+    //   </div>
 
-        //   <div class="form-outline mb-4">
-        //     <input type="password" id="form1Example2" class="form-control" />
-        //     <label class="form-label" for="form1Example2">
-        //       Password
-        //     </label>
-        //   </div>
+    //   <div class="form-outline mb-4">
+    //     <input type="password" id="form1Example2" class="form-control" />
+    //     <label class="form-label" for="form1Example2">
+    //       Password
+    //     </label>
+    //   </div>
 
-        //   <div class="row mb-4">
-        //     <div class="col d-flex justify-content-center">
-        //       <div class="form-check">
-        //         <input
-        //           class="form-check-input"
-        //           type="checkbox"
-        //           value=""
-        //           id="form1Example3"
-        //           checked
-        //         />
-        //         <label class="form-check-label" for="form1Example3">
-        //           {" "}
-        //           Remember me{" "}
-        //         </label>
-        //       </div>
-        //     </div>
+    //   <div class="row mb-4">
+    //     <div class="col d-flex justify-content-center">
+    //       <div class="form-check">
+    //         <input
+    //           class="form-check-input"
+    //           type="checkbox"
+    //           value=""
+    //           id="form1Example3"
+    //           checked
+    //         />
+    //         <label class="form-check-label" for="form1Example3">
+    //           {" "}
+    //           Remember me{" "}
+    //         </label>
+    //       </div>
+    //     </div>
 
-        //     <div class="col">
-        //       <a href="#!">Forgot password?</a>
-        //     </div>
-        //   </div>
+    //     <div class="col">
+    //       <a href="#!">Forgot password?</a>
+    //     </div>
+    //   </div>
 
-        //   <button type="submit" class="signinBtn btn btn-primary btn-block">
-        //     Sign Up
-        //   </button>
-        // </form>
+    //   <button type="submit" class="signinBtn btn btn-primary btn-block">
+    //     Sign Up
+    //   </button>
+    // </form>
     //   </div>
 
     //   {/* -----------------login------------------ */}
 
-
-
-
     //   {/* <div className="login animate__animated animate__fadeInRight"> */}
     //   <div className="container col-3">
-        // <h1 className="title">Login</h1>
-        // <form>
-        //   <div class="form-outline mb-4">
-        //     <input type="email" id="form1Example1" class="form-control" />
-        //     <label class="form-label" for="form1Example1">
-        //       Email address
-        //     </label>
-        //   </div>
+    // <h1 className="title">Login</h1>
+    // <form>
+    //   <div class="form-outline mb-4">
+    //     <input type="email" id="form1Example1" class="form-control" />
+    //     <label class="form-label" for="form1Example1">
+    //       Email address
+    //     </label>
+    //   </div>
 
-        //   <div class="form-outline mb-4">
-        //     <input type="password" id="form1Example2" class="form-control" />
-        //     <label class="form-label" for="form1Example2">
-        //       Password
-        //     </label>
-        //   </div>
+    //   <div class="form-outline mb-4">
+    //     <input type="password" id="form1Example2" class="form-control" />
+    //     <label class="form-label" for="form1Example2">
+    //       Password
+    //     </label>
+    //   </div>
 
-        //   <div class="row mb-4">
-        //     <div class="col d-flex justify-content-center">
-        //       <div class="form-check">
-        //         <input
-        //           class="form-check-input"
-        //           type="checkbox"
-        //           value=""
-        //           id="form1Example3"
-        //           checked
-        //         />
-        //         <label class="form-check-label" for="form1Example3">
-        //           {" "}
-        //           Remember me{" "}
-        //         </label>
-        //       </div>
-        //     </div>
+    //   <div class="row mb-4">
+    //     <div class="col d-flex justify-content-center">
+    //       <div class="form-check">
+    //         <input
+    //           class="form-check-input"
+    //           type="checkbox"
+    //           value=""
+    //           id="form1Example3"
+    //           checked
+    //         />
+    //         <label class="form-check-label" for="form1Example3">
+    //           {" "}
+    //           Remember me{" "}
+    //         </label>
+    //       </div>
+    //     </div>
 
-        //     <div class="col">
-        //       <a href="#!">Forgot password?</a>
-        //     </div>
-        //   </div>
-        //   <button type="submit" class=" signinBtn btn btn-primary btn-block">
-        //     Sign in
-        //   </button>
-        // </form>
+    //     <div class="col">
+    //       <a href="#!">Forgot password?</a>
+    //     </div>
+    //   </div>
+    //   <button type="submit" class=" signinBtn btn btn-primary btn-block">
+    //     Sign in
+    //   </button>
+    // </form>
     //     </div>
     //   {/* </div> */}
     //   </div>
--- models/user.js
+++ models/user.js
@@ -2,67 +2,82 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		required: "First Name is Required",
-	},
+  firstName: {
+    type: String,
+    trim: true,
+    required: "First Name is Required",
+  },
 
-	lastName: {
-		type: String,
-		trim: true,
-		required: "Last Name is Required",
-	},
+  lastName: {
+    type: String,
+    trim: true,
+    required: "Last Name is Required",
+  },
 
-	password: {
-		type: String,
-		trim: true,
-		required: "Password is Required",
-		validate: [({ length }) => length >= 6, "Password should be longer."],
-	},
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [({ length }) => length >= 6, "Password should be longer."],
+  },
 
-	email: {
-		type: String,
-		unique: true,
-		match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-	},
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
 
-	userCreated: {
-		type: Date,
-		default: Date.now,
-	},
+  userCreated: {
+    type: Date,
+    default: Date.now,
+  },
 
-	projectsInProgress: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Project",
-		},
-	],
+  projectsInProgress: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+    },
+  ],
 
-	projectsComplete: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: "Project",
-			// deployed_url: String
-		},
-	],
+  projectsComplete: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      // deployed_url: String
+    },
+  ],
 
-	lastUpdated: Date,
+  lastUpdated: Date,
 
-	fullName: String,
+  fullName: String,
 });
 
 UserSchema.methods.setFullName = function () {
-	this.fullName = `${this.firstName} ${this.lastName}`;
-	return this.fullName;
+  this.fullName = `${this.firstName} ${this.lastName}`;
+  return this.fullName;
 };
 
 UserSchema.methods.lastUpdatedDate = function () {
-	this.lastUpdated = Date.now();
-	return this.lastUpdated;
+  this.lastUpdated = Date.now();
+  return this.lastUpdated;
 };
 
 const User = mongoose.model("User", UserSchema);
+
+
+// ! bcrypt password function here
+
+
+function CheckPassword(inputtxt) {
+  var passw = user.password;
+  if (inputtxt.value.match(passw)) {
+    alert("Correct Password");
+    return true;
+  } else {
+    alert("Wrong Password...!");
+    return false;
+  }
+}
 
 module.exports = User;
 
