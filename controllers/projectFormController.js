9530cdd31e596a95db449316c33306aa353eb20d
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-30 01:57:17 -0400

    changed dbprojectForm to dbProjectForm

--- controllers/projectFormController.js
+++ controllers/projectFormController.js
@@ -3,35 +3,35 @@ const db = require("../models");
 // Defining methods for the projectFormController
 module.exports = {
   find: function(req, res) {
-    db.projectForm
+    db.ProjectForm
       .find(req.query)
       .sort({ date: -1 })
-      .then(dbprojectForm => res.json(dbprojectForm))
+      .then(dbProjectForm => res.json(dbProjectForm))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.projectForm
+    db.ProjectForm
       .findById(req.params.id)
-      .then(dbprojectForm => res.json(dbprojectForm))
+      .then(dbProjectForm => res.json(dbProjectForm))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.projectForm
+    db.ProjectForm
       .create(req.body)
-      .then(dbprojectForm => res.json(dbprojectForm))
+      .then(dbProjectForm => res.json(dbProjectForm))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.projectForm
+    db.ProjectForm
       .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbprojectForm => res.json(dbprojectForm))
+      .then(dbProjectForm => res.json(dbProjectForm))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.projectForm
+    db.ProjectForm
       .findById({ _id: req.params.id })
-      .then(dbprojectForm => dbprojectForm.remove())
-      .then(dbprojectForm => res.json(dbprojectForm))
+      .then(dbProjectForm => dbProjectForm.remove())
+      .then(dbProjectForm => res.json(dbProjectForm))
       .catch(err => res.status(422).json(err));
   }
 };
