e6d1166f6d5904aad4c385c9d039178438314e05
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-30 04:34:30 -0400

    rerouting completed project details page

--- client/src/App.js
+++ client/src/App.js
@@ -9,29 +9,35 @@ import ReactProjects from "./pages/ReactProjects";
 import AlgProjects from "./pages/AlgProjects";
 import Contact from "./pages/Contact/Contact";
 import CompletedProjects from "./pages/CompletedProjects";
+// import ProjectForm from "./components/ProjectForm/index";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer/Footer';
 
 
+
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/jsprojects" component={JavaScriptProjects} />
-          <Route exact path="/reactprojects" component={ReactProjects} />
-          <Route exact path="/algprojects" component={AlgProjects} />
-          <Route exact path="/completedprojects" component-={CompletedProjects}/>
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      {/* </div> */}
-      <Footer />
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/jsprojects" component={JavaScriptProjects} />
+      <Route exact path="/reactprojects" component={ReactProjects} />
+      <Route exact path="/algprojects" component={AlgProjects} />
+      <Route exact path="/projectForm/:id">
+        <CompletedProjects />
+        </Route>
+      <Route exact path="/contact" component={Contact} />
+    </Switch>
+    {/* </div> */}
+    <Footer />
     </Router>
   );
 }
 
 export default App;
+
+
