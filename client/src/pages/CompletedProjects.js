927757bf4184c0e536ff50223db715bab3e17fd6
Sopee Thong <sthong@Sopees-MBP.attlocal.net>
2021-05-30 03:46:20 -0400

    troubleshooting

--- client/src/pages/CompletedProjects.js
+++ client/src/pages/CompletedProjects.js
@@ -3,9 +3,10 @@ import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
 import API from "../utils/API";
+import Navbar1 from "../components/Navbar1/Navbar1";
 
 function CompletedProjects(props) {
-  const [ProjectForm, setProjectForm] = useState({})
+  const [projectForm, setProjectForm] = useState({})
 
  
   const {id} = useParams()
@@ -16,34 +17,37 @@ function CompletedProjects(props) {
   }, [id])
 
   return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
+    
+    <Container fluid>
+      <Navbar1/>
+      <Row>
+        <Col size="md-12">
           <Jumbotron>
-              <h1>
-                {ProjectForm.projectname} by {ProjectForm.username}
-              </h1>
-              </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>URL</h1>
-              <p>
-                {ProjectForm.url}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/profile">← Back to My Profile</Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+            <h1>
+              {projectForm.projectname} by {projectForm.username}
+            </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-10 md-offset-1">
+          <article>
+            <h1>URL</h1>
+            <p>
+              {projectForm.url}
+            </p>
+          </article>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-2">
+          <Link to="/profile">← Back to My Profile</Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 
 export default CompletedProjects;
+
